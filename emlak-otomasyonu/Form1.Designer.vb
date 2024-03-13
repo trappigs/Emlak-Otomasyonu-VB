105709dbe3168ef2d4be5e21a235a0df6fcccb6e
Muhammed Hatay <46846228+trappigs@users.noreply.github.com>
2024-03-13 15:28:32 +0300

    tasarım 1

--- emlak-otomasyonu/Form1.Designer.vb
+++ emlak-otomasyonu/Form1.Designer.vb
@@ -22,10 +22,222 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Label1 = New Label()
+        ComboBox1 = New ComboBox()
+        Label2 = New Label()
+        ComboBox2 = New ComboBox()
+        Label3 = New Label()
+        ComboBox3 = New ComboBox()
+        Label4 = New Label()
+        ComboBox4 = New ComboBox()
+        Label5 = New Label()
+        ComboBox5 = New ComboBox()
+        Label6 = New Label()
+        ComboBox6 = New ComboBox()
+        Label7 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label8 = New Label()
+        TextBox3 = New TextBox()
+        Label9 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(72, 25)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(39, 20)
+        Label1.TabIndex = 3
+        Label1.Text = "Kaks"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(72, 60)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(80, 28)
+        ComboBox1.TabIndex = 2
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(72, 109)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(53, 20)
+        Label2.TabIndex = 5
+        Label2.Text = "Gabari"
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Location = New Point(72, 144)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(80, 28)
+        ComboBox2.TabIndex = 4
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(72, 192)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(51, 20)
+        Label3.TabIndex = 7
+        Label3.Text = "Cephe"
+        ' 
+        ' ComboBox3
+        ' 
+        ComboBox3.FormattingEnabled = True
+        ComboBox3.Location = New Point(72, 227)
+        ComboBox3.Name = "ComboBox3"
+        ComboBox3.Size = New Size(80, 28)
+        ComboBox3.TabIndex = 6
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(72, 277)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(78, 20)
+        Label4.TabIndex = 9
+        Label4.Text = "Emlak Tipi"
+        ' 
+        ' ComboBox4
+        ' 
+        ComboBox4.FormattingEnabled = True
+        ComboBox4.Location = New Point(72, 312)
+        ComboBox4.Name = "ComboBox4"
+        ComboBox4.Size = New Size(80, 28)
+        ComboBox4.TabIndex = 8
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(216, 109)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(96, 20)
+        Label5.TabIndex = 11
+        Label5.Text = "İmar Çeşitleri"
+        ' 
+        ' ComboBox5
+        ' 
+        ComboBox5.FormattingEnabled = True
+        ComboBox5.Location = New Point(216, 144)
+        ComboBox5.Name = "ComboBox5"
+        ComboBox5.Size = New Size(80, 28)
+        ComboBox5.TabIndex = 10
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(216, 192)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(89, 20)
+        Label6.TabIndex = 13
+        Label6.Text = "İşyeri Türleri"
+        ' 
+        ' ComboBox6
+        ' 
+        ComboBox6.FormattingEnabled = True
+        ComboBox6.Location = New Point(216, 227)
+        ComboBox6.Name = "ComboBox6"
+        ComboBox6.Size = New Size(80, 28)
+        ComboBox6.TabIndex = 12
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(443, 126)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(39, 20)
+        Label7.TabIndex = 14
+        Label7.Text = "Alan"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(443, 159)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 15
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(443, 241)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 17
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(443, 208)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(84, 20)
+        Label8.TabIndex = 16
+        Label8.Text = "Satılık Fiyat"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(443, 329)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 27)
+        TextBox3.TabIndex = 19
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(443, 296)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(85, 20)
+        Label9.TabIndex = 18
+        Label9.Text = "Kiralık Fiyat"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1139, 450)
+        Controls.Add(TextBox3)
+        Controls.Add(Label9)
+        Controls.Add(TextBox2)
+        Controls.Add(Label8)
+        Controls.Add(TextBox1)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(ComboBox6)
+        Controls.Add(Label5)
+        Controls.Add(ComboBox5)
+        Controls.Add(Label4)
+        Controls.Add(ComboBox4)
+        Controls.Add(Label3)
+        Controls.Add(ComboBox3)
+        Controls.Add(Label2)
+        Controls.Add(ComboBox2)
+        Controls.Add(Label1)
+        Controls.Add(ComboBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ComboBox5 As ComboBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ComboBox6 As ComboBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label9 As Label
 
 End Class
--- emlak-otomasyonu/Form1.vb
+++ emlak-otomasyonu/Form1.vb
@@ -1,3 +1,45 @@
-﻿Public Class Form1
+﻿Imports System.Data.Common
+Imports System.Data.SqlClient
+
+
+Public Class Form1
+
+    Dim connectionString As String = "Data Source=SERVER_NAME;Initial Catalog=DATABASE_NAME;User ID=USERNAME;Password=PASSWORD"
+
+
+    Sub MySub()
+
+        ' Veritabanı bağlantısı oluştur
+        Using connection As New SqlConnection(connectionString)
+            ' SQL sorgusu
+            Dim query As String = "SELECT columnName FROM tableName"
+
+            ' SqlCommand nesnesi oluştur
+            Dim command As New SqlCommand(query, connection)
+
+            Try
+                ' Veri tablosu oluştur
+                Dim dataTable As New DataTable()
+
+                ' Veri adaptörü oluştur
+                Dim dataAdapter As New SqlDataAdapter(command)
+
+                ' Verileri veri tablosuna doldur
+                dataAdapter.Fill(dataTable)
+
+                ' ComboBox'ın DataSource özelliğine veri tablosunu ata
+                ComboBox1.DataSource = dataTable
+
+                ' ComboBox'ta gösterilecek sütun adını belirt
+                ComboBox1.DisplayMember = "columnName"
+            Catch ex As Exception
+                MessageBox.Show("Veri okuma hatası: " & ex.Message)
+            End Try
+        End Using
+
+    End Sub
+
+
+
 
 End Class
